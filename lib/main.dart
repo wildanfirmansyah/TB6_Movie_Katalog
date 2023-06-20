0198878c0c3bf7cd8547ec509daf00d3d9522c68
wildanfirmansyah <129596017+wildanfirmansyah@users.noreply.github.com>
2023-06-21 01:52:29 +0700

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,50 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_native_splash/flutter_native_splash.dart';
+import 'package:provider/provider.dart';
+import 'package:yt_flutter_movie_db/injector.dart';
+import 'package:yt_flutter_movie_db/movie/pages/movie_page.dart';
+import 'package:yt_flutter_movie_db/movie/providers/movie_get_discover_provider.dart';
+import 'package:yt_flutter_movie_db/movie/providers/movie_get_now_playing_provider.dart';
+import 'package:yt_flutter_movie_db/movie/providers/movie_get_top_rated_provider.dart';
+
+import 'movie/providers/movie_serach_provider.dart';
+
+void main() {
+  WidgetsBinding widgetsBinding = WidgetsFlutterBinding.ensureInitialized();
+  FlutterNativeSplash.preserve(widgetsBinding: widgetsBinding);
+  setup();
+  runApp(const App());
+  FlutterNativeSplash.remove();
+}
+
+class App extends StatelessWidget {
+  const App({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MultiProvider(
+      providers: [
+        ChangeNotifierProvider(
+          create: (_) => sl<MovieGetDiscoverProvider>(),
+        ),
+        ChangeNotifierProvider(
+          create: (_) => sl<MovieGetTopRatedProvider>(),
+        ),
+        ChangeNotifierProvider(
+          create: (_) => sl<MovieGetNowPlayingProvider>(),
+        ),
+        ChangeNotifierProvider(
+          create: (_) => sl<MovieSearchProvider>(),
+        ),
+      ],
+      child: MaterialApp(
+        title: 'Movie DB',
+        theme: ThemeData(
+          primarySwatch: Colors.blue,
+        ),
+        home: const MoviePage(),
+        debugShowCheckedModeBanner: false,
+      ),
+    );
+  }
+}
